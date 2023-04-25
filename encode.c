41ef2fd4689d8fa70608f4bd403ea42439f7995d
John Hoffman <johnhoffman@ucla.edu>
2023-04-25 16:37:59 -0700

    Encode prints packet to stdout

--- encode.c
+++ encode.c
@@ -9,24 +9,25 @@
 #include <string.h>
 #include <stdio.h>
 
+// packet size in bytes
 #define PKT_SIZE 128
 #define HDR_SIZE 6
 #define PAYLOAD_SIZE 122 // PKT_SIZE - HDR_SIZE
 
 struct hoffcsp_pkt {
-    uint32_t preamble;
+    uint32_t preamble; 
     uint16_t length;
-    char payload[PAYLOAD_SIZE];
+    uint8_t payload[PAYLOAD_SIZE];
 };
 
 void buildPkt(struct hoffcsp_pkt* pkt, uint16_t length, const char* payload){
-    pkt->preamble = 0xFFFFFFFF;
+    pkt->preamble = (uint32_t) 0xFFFFFFFF ;
     pkt->length = length;
     memcpy(pkt->payload, payload, length);
 }
 
-void prettyPrintBytes(struct hoffcsp_pkt* pkt_ptr){
-    struct hoffcsp_pkt pkt = *pkt_ptr;
+void prettyPrintBytes(struct hoffcsp_pkt* pktPtr){
+    struct hoffcsp_pkt pkt = *pktPtr;
     // pretty print packet bytes
     printf("%zu bytes:\n", sizeof(pkt));
     for (int i = 0; i < sizeof(pkt); i++) {
@@ -35,13 +36,20 @@ void prettyPrintBytes(struct hoffcsp_pkt* pkt_ptr){
     printf("\n");
 }
 
+void printPkt(struct hoffcsp_pkt* pkt){
+    fwrite (pkt, sizeof(struct hoffcsp_pkt), 1, stdout);
+}
+
 int main() {
+    char buffer[PAYLOAD_SIZE] = {0};
     struct hoffcsp_pkt pkt = {0};
-    char buf[] = "hello world";
-    int length = sizeof(buf);
-    buildPkt(&pkt, length, buf);
-    
-    prettyPrintBytes(&pkt);
+
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, sizeof(char), PAYLOAD_SIZE, stdin)) > 0) {
+        buildPkt(&pkt, bytesRead, buffer);
+        // prettyPrintBytes(&pkt);
+        printPkt(&pkt);
+    }
     
     return 0;
 }
