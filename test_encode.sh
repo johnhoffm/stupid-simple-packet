ace09b8f9ef7754e89449208c33d7da61224e503
John Hoffman <johnhoffman@ucla.edu>
2023-04-25 17:56:58 -0700

    forgot in previous commit

--- test_encode.sh
+++ test_encode.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 make
-cat lorem_ipsum.txt | ./encode
+# encode file into packets then decode packets back into file
+cat lorem_ipsum.txt | ./encode | ./decode > out.txt
+# verify that there is no difference between original file and reconstructed packets
+diff lorem_ipsum.txt out.txt
+rm -f out.txt
