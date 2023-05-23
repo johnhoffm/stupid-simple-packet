0f864a6f32d6db8bc1484210d126f78ea021fed0
John Hoffman <johnhoffman@ucla.edu>
2023-05-23 16:19:34 -0700

    Encode stores output packets as separate files. Decode is broken.

--- encode.c
+++ encode.c
@@ -1,11 +1,16 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 
 // packet size in bytes
 #define PKT_SIZE 128
 #define HDR_SIZE 6
 #define PAYLOAD_SIZE 122 // PKT_SIZE - HDR_SIZE
+
+const char* OUT_DIR = "packets/";
 
 struct hoffcsp_pkt {
     uint32_t preamble; 
@@ -29,19 +34,39 @@ void prettyPrintBytes(struct hoffcsp_pkt* pktPtr){
     printf("\n");
 }
 
+void savePktToFile(struct hoffcsp_pkt* pkt, FILE* fp){
+    fwrite (pkt, sizeof(struct hoffcsp_pkt), 1, fp);
+}
+
 void printPkt(struct hoffcsp_pkt* pkt){
-    fwrite (pkt, sizeof(struct hoffcsp_pkt), 1, stdout);
+    savePktToFile(pkt, stdout);
 }
 
 int main() {
     char buffer[PAYLOAD_SIZE] = {0};
     struct hoffcsp_pkt pkt = {0};
 
+    FILE *fp;
+    char fileName[100]; // can overflow if very many packets
+    int pktNum = 0;
     size_t bytesRead;
+
+    if (mkdir(OUT_DIR, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1 && errno != EEXIST){
+        perror("Cannot create directory for packets.");
+        return 1;
+    }
+
     while ((bytesRead = fread(buffer, sizeof(char), PAYLOAD_SIZE, stdin)) > 0) {
         buildPkt(&pkt, bytesRead, buffer);
+        sprintf(&fileName, "%s%d.hoff", OUT_DIR, pktNum);
+
+        fp = fopen(fileName, "wb+");
+        savePktToFile(&pkt, fp);
+        fclose(fp);
+
         // prettyPrintBytes(&pkt);
-        printPkt(&pkt);
+
+        pktNum = pktNum + 1;
     }
     
     return 0;
